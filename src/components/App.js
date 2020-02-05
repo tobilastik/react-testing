852f4d85e2cdccde88bd46a608d660234e6f8932
tobilastik <rajioluwatobih@gmail.com>
2020-02-05 09:28:08 +0100

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import TweetBox from './TweetBox';
 import TweetList from './TweetList';
+import Header from './Header';
 
 export default () => {
   return (
     <div>
+      <Header />
       <TweetBox />
       <TweetList />
+
     </div>
   );
 };
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+const Header = props => {
+  return (
+    <div className="App">
+      Header
+    </div>
+  );
+};
+
+export default Header;
